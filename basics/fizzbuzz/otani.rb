7c5145c66e996e57d3b195d97332c475324c48f9
shinjifinc <shinji.otani.fb@gmail.com>
2015-02-05 11:51:25 +0900

    Implement new fizzbuzz

--- basics/fizzbuzz/otani.rb
+++ basics/fizzbuzz/otani.rb
@@ -1,10 +1,10 @@
 100.times do |i|
 
-  if i%15 == 0 
+  if (i%15).zero? 
     puts 'fizzbuzz'
-  elsif i%3 == 0
+  elsif (i%3).zero? 
     puts 'fizz'
-  elsif i%5 == 0
+  elsif (i%5).zero? 
     puts 'buzz'
   else
     puts i
