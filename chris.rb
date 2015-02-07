d52888a8e3391109c13ae7aa98fdd006f91d7a97
chrisfinc <chrischris0801@gmail.com>
2015-02-07 15:50:18 +0900

    Remove comment

--- chris.rb
+++ chris.rb
@@ -1,4 +1,3 @@
-#2nd
 (1..100).each do |i|
   if i % 15 == 0
     puts "FizzBuzz"
@@ -10,20 +9,3 @@
     puts i
   end
 end
-
-=begin
-
-#1st
-for i in 1..100 do
-  if i % 3 ==0 && i % 5 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else
-    puts i
-  end
-end
-
-=end
