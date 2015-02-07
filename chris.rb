e9560259996681f165f475dff4a40df28a159e25
chrisfinc <chrischris0801@gmail.com>
2015-02-07 15:37:48 +0900

    "for" is not used in the 2nd version.

--- chris.rb
+++ chris.rb
@@ -1,4 +1,17 @@
 #2nd
+(1..100).each do |i|
+  if i % 15 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i
+  end
+end
+
+=begin
 
 #1st
 for i in 1..100 do
@@ -12,3 +25,5 @@ for i in 1..100 do
     puts i
   end
 end
+
+=end
